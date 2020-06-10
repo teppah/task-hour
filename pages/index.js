fdc15da9fdf0cdcf44af89ffba07a9905c565420
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-10 11:59:37 -0400

    Create initial layout of app

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-import Head from "next/head";
-const Index = () => {
-  return <div>Hello</div>;
-};
-export default Index;
---
+++ pages/index.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+const Index = () => {
+  return (
+    <div className="content">
+      <Head>
+        <title>Task Hour</title>
+      </Head>
+      <div className="vertical-bar">
+        <div>Calendar</div>
+        <div>TaskListView</div>
+      </div>
+      <div id="summary">SumaryView</div>
+      <div className="vertical-bar">
+        <div>ButtonArray</div>
+        <div>PomordoTimer</div>
+        <div>DetailedTaskView</div>
+      </div>
+      <style jsx>{`
+        div {
+          @apply border-dotted border border-red-400;
+        }
+        .content {
+          @apply flex flex-row;
+          @apply border-purple-800 border-2;
+        }
+        .content > * {
+          @apply h-screen;
+        }
+        #summary {
+          @apply flex-grow;
+        }
+        .vertical-bar {
+          @apply flex flex-col;
+        }
+        .vertical-bar > * {
+          @apply flex-1;
+        }
+      `}</style>
+    </div>
+  );
+};
+export default Index;
