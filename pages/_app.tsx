807fe0b27db055023ad5627624a53235ec16f512
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-12 14:40:55 -0400

    Add Pullstate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "css/tailwind.css";
+import { PullstateCore } from "data/pullstate/PullstateCore";
+import { PullstateProvider } from "pullstate";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const instance = PullstateCore.instantiate({ ssr: true });
+  return (
+    <PullstateProvider instance={instance}>
+      <Component {...pageProps} />
+    </PullstateProvider>
+  );
 };
 
 export default App;
