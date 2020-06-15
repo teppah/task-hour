488ddad7e2ae35215e2cd02a9c941efc93ba13af
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-15 14:35:12 -0400

    Update task definition to class

--- data/types/Task.ts
+++ data/types/Task.ts
@@ -1,6 +1,14 @@
-type Task = {
+class Task {
   taskId: number;
   title: string;
   description: string;
-};
+  date: Date;
+  constructor(taskId: number, title: string, description: string, date: Date) {
+    this.taskId = taskId;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+  }
+}
+
 export default Task;
