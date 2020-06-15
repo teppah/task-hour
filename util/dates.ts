f35ce2e6002bb97098076b63d4b6847670330c64
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-15 14:35:56 -0400

    Add weekday id to string util

---
+++ util/dates.ts
@@ -0,0 +1,18 @@
+export function getDayName(dayId: number) {
+  switch (dayId) {
+    case 0:
+      return "sunday";
+    case 1:
+      return "monday";
+    case 2:
+      return "tuesday";
+    case 3:
+      return "wednesday";
+    case 4:
+      return "thursday";
+    case 5:
+      return "friday";
+    case 6:
+      return "saturday";
+  }
+}
