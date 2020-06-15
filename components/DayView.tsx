43cae63ea0ec0affb0bf4cccfc917fd3fe2725c0
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-15 15:03:30 -0400

    Implement display task by the hour

--- components/DayView.tsx
+++ components/DayView.tsx
@@ -1,27 +1,52 @@
 import TimeSlice from "components/TimeSlice";
 import Task from "data/types/Task";
 import { PullstateCore } from "data/pullstate/PullstateCore";
-import { range } from "lodash";
+import { range, capitalize } from "lodash";
+import { getDayName } from "util/dates";
+import {
+  addDays,
+  getDate,
+  isSameDay,
+  compareAsc,
+  isSameHour,
+  addHours,
+} from "date-fns";
 type Props = {
-  dayName: string;
+  day: number;
 };
 
-const DayView = ({ dayName }: Props) => {
+const DayView = ({ day }: Props) => {
   // 1. read the currentWeek here
   // 2. get an array of all the tasks that need to be completed during this
   //    week based on the date
   // 3. sort the array by date DESCENDING
   // 4. go through each hour: when task is between timesliceHour and
   //    timesliceHour + 1, pass the task TimeSlice
-  // 5. array.pop() to reduce array size;
-  const { TaskStore } = PullstateCore.useStores();
-  const tasks = TaskStore.useState((s) => s.tasks);
+  // 5. TODO: array.pop() to reduce array size;
+  const dayName = capitalize(getDayName(day));
+  const { DateStore, CurrentTaskStore } = PullstateCore.useStores();
+  const currentWeekDate = DateStore.useState((s) => s.currentWeekDate);
+  const currentDate = addDays(currentWeekDate, day);
+  const currentDayDate = getDate(currentDate);
+
+  const weekTasks = CurrentTaskStore.useState((s) => s.currentTasks);
+  const tasks = weekTasks.filter((task) => {
+    return isSameDay(currentDate, task.date);
+  });
+
   return (
     <section className="day-view">
+      <h1>{currentDayDate}</h1>
       <h1>{dayName}</h1>
-      {range(24).map((i) => (
-        <TimeSlice task={i < 4 ? tasks[i % 4] : null} />
-      ))}
+      {range(24).map((i) => {
+        const foundTasks = tasks.filter((t) =>
+          isSameHour(t.date, addHours(currentDate, i))
+        );
+        // For now, assume there is only one task per hour
+        return (
+          <TimeSlice task={foundTasks.length > 0 ? foundTasks[0] : null} />
+        );
+      })}
       <style jsx>{`
         .day-view {
           @apply flex flex-col items-center;
--- components/SummaryView.tsx
+++ components/SummaryView.tsx
@@ -1,7 +1,9 @@
 import DayView from "components/DayView";
+import { PullstateCore } from "data/pullstate/PullstateCore";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import range from "lodash/range";
+import { isSameWeek } from "date-fns";
 
 type Props = {};
 const SummaryView = ({}: Props) => {
@@ -10,27 +12,31 @@ const SummaryView = ({}: Props) => {
   // pulls data from a centralized AllTasksStore
   // 3. get all the relevant dates and month and display them on the top
   // 4. pass each day's date to every DayView
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  const days = [0, 1, 2, 3, 4, 5, 6];
+
+  const { CurrentTaskStore, DateStore, TaskStore } = PullstateCore.useStores();
+  const currentWeek = DateStore.useState((s) => s.currentWeekDate);
+  const tasks = TaskStore.useState((s) => s.tasks);
+  const filtered = tasks.filter((task) => {
+    const taskDate = task.date;
+    return isSameWeek(currentWeek, taskDate);
+  });
+  CurrentTaskStore.update((s) => {
+    s.currentTasks = filtered;
+  });
 
   return (
     <DndProvider backend={HTML5Backend}>
       <section className="summary-view">
         <div className="time-col">
           <div></div>
+          <div></div>
           {range(24).map((i) => (
             <div>{i}</div>
           ))}
         </div>
         {days.map((day) => (
-          <DayView dayName={day} />
+          <DayView day={day} />
         ))}
         <style jsx>{`
           .summary-view {
