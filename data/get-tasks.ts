24cbea98deae550e9743559dedb0dcb45cf7d334
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-16 16:58:00 -0400

    Replace id with random nanoid

--- data/get-tasks.ts
+++ data/get-tasks.ts
@@ -1,27 +1,33 @@
 import { set, subWeeks, subDays } from "date-fns";
+import { nanoid } from "nanoid";
 import Task, { createTask } from "./Task";
 const now = new Date();
 const task1 = createTask(
-  "0",
+  nanoid(),
   "Task 1",
   "Description 1",
   set(now, { hours: 22 })
 );
 const task2 = createTask(
-  "1",
+  nanoid(),
   "Task 2",
   "Description 2",
   set(now, { hours: 10 })
 );
-const task3 = createTask("2", "Last Week 1", "Description 3", subWeeks(now, 1));
+const task3 = createTask(
+  nanoid(),
+  "Last Week 1",
+  "Description 3",
+  subWeeks(now, 1)
+);
 const task4 = createTask(
-  "3",
+  nanoid(),
   "Yesterday task 1",
   "Description 4",
   subDays(now, 1)
 );
 const task5 = createTask(
-  "4",
+  nanoid(),
   "Yesterday task 2",
   "Description 5",
   subDays(set(now, { hours: 6 }), 1)
