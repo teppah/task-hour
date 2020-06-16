df89042af1218537e0a58f9143676f4b01411f3b
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-16 14:47:51 -0400

    Add state to store currently active item

--- data/redux/slice/taskSlice.ts
+++ data/redux/slice/taskSlice.ts
@@ -8,16 +8,23 @@ const taskSlice = createSlice({
   initialState: {
     tasks: getTasks(),
     currentViewTasks: [],
+    selectedTask: null,
   },
   reducers: {
     setCurrentTasks: (state, action: PayloadAction<Task[]>) => {
       state.currentViewTasks = action.payload;
     },
+    setSelectedTask: (state, action: PayloadAction<Task>) => {
+      state.selectedTask = action.payload;
+    },
   },
 });
 
 export const selectTasks = (state: RootState) => state.tasks.tasks;
-export const selectCurrentTasks = (state: RootState) =>
+// TODO: fix automatic type inference
+export const selectCurrentTasks = (state: RootState): Task[] =>
   state.tasks.currentViewTasks;
-export const { setCurrentTasks } = taskSlice.actions;
+export const selectSelectedTask = (state: RootState): Task =>
+  state.tasks.selectedTask;
+export const { setCurrentTasks, setSelectedTask } = taskSlice.actions;
 export default taskSlice.reducer;
