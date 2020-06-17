6c725c0a2a9d684d880b73f7c737ae09cd18b7e3
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-17 16:25:24 -0400

    Add custom tileContent based on number of tasks that day

--- components/CalendarView.tsx
+++ components/CalendarView.tsx
@@ -4,9 +4,12 @@ import {
   selectSelectedDate,
   setSelectedDate,
 } from "data/redux/slice/dateSlice";
+import { selectTasks } from "data/redux/slice/taskSlice";
+import { isSameDay } from "date-fns";
 
 const CalendarView = () => {
   const selectedDate = useSelector(selectSelectedDate);
+  const allTasks = useSelector(selectTasks);
   const dispatch = useDispatch();
 
   return (
@@ -14,10 +17,21 @@ const CalendarView = () => {
       <Calendar
         value={selectedDate}
         onClickDay={(day) => dispatch(setSelectedDate(day))}
-        locale={"en-US"}
+        calendarType={"US"}
+        tileContent={(props) => {
+          const { date } = props;
+          const todayTasks = allTasks.filter((t) => isSameDay(t.date, date));
+          return <p>{todayTasks.map((t) => "*")}</p>;
+        }}
       />
       <style jsx>{`
         section {
+        }
+      `}</style>
+      <style jsx global>{`
+        p {
+          @apply text-purple-500 font-bold;
+          @apply h-1;
         }
       `}</style>
     </section>
