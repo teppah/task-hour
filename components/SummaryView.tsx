0119e76a023911df1703742aa9626cb426090990
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-18 16:12:09 -0400

    Wrap the whole index with DndProvider instead of only SummaryView

--- components/SummaryView.tsx
+++ components/SummaryView.tsx
@@ -1,6 +1,4 @@
 import DayView from "components/DayView";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import range from "lodash/range";
 import { isSameWeek } from "date-fns";
 import { useSelector, useDispatch } from "react-redux";
@@ -29,38 +27,36 @@ const SummaryView = ({}: Props) => {
   dispatch(setCurrentTaskIds(filteredIds));
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <section className="summary-view">
-        <div className="time-col">
-          <div></div>
-          <div></div>
-          {range(24).map((i) => (
-            <div>{i}</div>
-          ))}
-        </div>
-        {days.map((day) => (
-          <DayView day={day} />
+    <section className="summary-view">
+      <div className="time-col">
+        <div></div>
+        <div></div>
+        {range(24).map((i) => (
+          <div>{i}</div>
         ))}
-        <style jsx>{`
-          .summary-view {
-            @apply flex flex-row;
-            @apply h-full w-full;
-          }
-          .time-col {
-            @apply flex flex-col;
-            @apply flex-none h-full;
-          }
-          .time-col > div:first-child {
-            @apply h-5;
-          }
-          .time-col > div {
-            @apply text-center;
-            @apply mx-2;
-            @apply h-8;
-          }
-        `}</style>
-      </section>
-    </DndProvider>
+      </div>
+      {days.map((day) => (
+        <DayView day={day} />
+      ))}
+      <style jsx>{`
+        .summary-view {
+          @apply flex flex-row;
+          @apply h-full w-full;
+        }
+        .time-col {
+          @apply flex flex-col;
+          @apply flex-none h-full;
+        }
+        .time-col > div:first-child {
+          @apply h-5;
+        }
+        .time-col > div {
+          @apply text-center;
+          @apply mx-2;
+          @apply h-8;
+        }
+      `}</style>
+    </section>
   );
 };
 export default SummaryView;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,58 @@
 import Head from "next/head";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import SummaryView from "components/SummaryView";
 import DetailedTaskView from "components/DetailedTaskView";
 import CalendarView from "components/CalendarView";
+import TaskListView from "components/ListView";
 const Index = () => {
   return (
-    <div className="content">
-      <Head>
-        <title>Task Hour</title>
-      </Head>
-      <div className="vertical-bar">
-        <div>
-          <CalendarView />
+    <DndProvider backend={HTML5Backend}>
+      <div className="content">
+        <Head>
+          <title>Task Hour</title>
+        </Head>
+        <div className="vertical-bar">
+          <div>
+            <CalendarView />
+          </div>
+          <div>
+            <TaskListView />
+          </div>
         </div>
-        <div>TaskListView</div>
-      </div>
-      <div id="summary">
-        <SummaryView />
-      </div>
-      <div className="vertical-bar">
-        <div>ButtonArray</div>
-        <div>PomordoTimer</div>
-        <div>
-          <DetailedTaskView />
+        <div id="summary">
+          <SummaryView />
         </div>
+        <div className="vertical-bar">
+          <div>ButtonArray</div>
+          <div>PomordoTimer</div>
+          <div>
+            <DetailedTaskView />
+          </div>
+        </div>
+        <style jsx>{`
+          div {
+            @apply border-dotted border border-red-400;
+          }
+          .content {
+            @apply flex flex-row;
+            @apply border-purple-800 border-2;
+          }
+          .content > * {
+            @apply h-screen;
+          }
+          #summary {
+            @apply flex-grow;
+          }
+          .vertical-bar {
+            @apply flex flex-col w-1/5;
+          }
+          .vertical-bar > * {
+            @apply flex-1;
+          }
+        `}</style>
       </div>
-      <style jsx>{`
-        div {
-          @apply border-dotted border border-red-400;
-        }
-        .content {
-          @apply flex flex-row;
-          @apply border-purple-800 border-2;
-        }
-        .content > * {
-          @apply h-screen;
-        }
-        #summary {
-          @apply flex-grow;
-        }
-        .vertical-bar {
-          @apply flex flex-col w-1/5;
-        }
-        .vertical-bar > * {
-          @apply w-full h-full;
-        }
-      `}</style>
-    </div>
+    </DndProvider>
   );
 };
 export default Index;
