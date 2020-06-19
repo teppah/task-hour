0c61ced7cd2478bc97637868715ca75745bdc7bf
Yi Feng Yang <yifeng@yfyang.dev>
2020-06-19 14:53:01 -0400

    Add view types for SummaryView

--- util/dates.ts
+++ util/dates.ts
@@ -16,3 +16,17 @@ export function getDayName(dayId: number) {
       return "saturday";
   }
 }
+
+export enum VIEWS {
+  FULL_WEEK = "fullweek",
+  WORK_WEEK = "workweek",
+}
+
+export function getDaysFromView(view: VIEWS) {
+  switch (view) {
+    case VIEWS.FULL_WEEK:
+      return [0, 1, 2, 3, 4, 5, 6];
+    case VIEWS.WORK_WEEK:
+      return [1, 2, 3, 4, 5];
+  }
+}
