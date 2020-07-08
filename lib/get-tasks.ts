935eba364c6c292b1774083749a7b8dcf1bef86f
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-08 10:10:46 -0400

    hardcode nanoids

--- lib/get-tasks.ts
+++ lib/get-tasks.ts
@@ -3,31 +3,31 @@ import { nanoid } from "nanoid";
 import Task, { createTask } from "./Task";
 const now = new Date();
 const task1 = createTask(
-  nanoid(),
+  "aa9WrxMnxAE52SzL24VPX",
   "Task 1",
   "Description 1",
   set(now, { hours: 22 })
 );
 const task2 = createTask(
-  nanoid(),
+  "IM9F0ml8uEeIQQJ7L4LQR",
   "Task 2",
   "Description 2",
   set(now, { hours: 10 })
 );
 const task3 = createTask(
-  nanoid(),
+  "Owjmbbhm0bsppYxJ-ydj2",
   "Last Week 1",
   "Description 3",
   subWeeks(now, 1)
 );
 const task4 = createTask(
-  nanoid(),
+  "D61l39Jpf1iRc3IawW98Z",
   "Yesterday task 1",
   "Description 4",
   subDays(now, 1)
 );
 const task5 = createTask(
-  nanoid(),
+  "JO807XfMTw-QjUgY5ewPA",
   "Yesterday task 2",
   "Description 5",
   subDays(set(now, { hours: 6 }), 1)
