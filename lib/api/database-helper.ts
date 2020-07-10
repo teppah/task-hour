35d87d3e71ec19930531e4861227780a534a7d13
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-10 18:26:15 -0400

    Add conversion function from Faunadb format to TS Task type

--- lib/api/database-helper.ts
+++ lib/api/database-helper.ts
@@ -1,7 +1,7 @@
 import serverClient from "./database";
 import Task from "lib/Task";
-import { query as q, QueryOptions } from "faunadb";
-import { title } from "process";
+import { query as q } from "faunadb";
+import { parseISO } from "date-fns";
 
 type ResType = {
   ref: any;
@@ -9,7 +9,7 @@ type ResType = {
   data: any;
 };
 const databaseHelper = {
-  createTask: async (task: Task): Promise<object> => {
+  createTask: async (task: Task): Promise<Task> => {
     const restOfData = {
       taskId: task.taskId,
       title: task.title,
@@ -25,19 +25,43 @@ const databaseHelper = {
         },
       })
     );
-    console.log(res);
-    return res;
+    const converted = await convertTask(res.data);
+    return converted;
   },
   getTask: async (taskId: string): Promise<Task> => {
     const res: ResType = await serverClient.query(
       q.Get(q.Match(q.Index("task_by_taskId"), taskId))
     );
-    const task: Task = res.data;
-    console.log(res);
-    console.log(typeof res.ref);
-    console.log(typeof res.data);
-    return task;
+    const converted = await convertTask(res.data);
+    return converted;
+  },
+  getTasks: async (): Promise<Task[]> => {
+    const res: ResType = await serverClient.query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection("tasks"))),
+        q.Lambda((document) => q.Get(document))
+      )
+    );
+    const tasks: Task[] = [];
+    for (const doc of res.data) {
+      const t = await convertTask(doc.data);
+      tasks.push(t);
+    }
+    return tasks;
   },
 };
+
+async function convertTask(task): Promise<Task> {
+  const start: string = await serverClient.query(q.ToString(task.startDate));
+  const end: string = await serverClient.query(q.ToString(task.endDate));
+  return {
+    taskId: task.taskId,
+    title: task.title,
+    description: task.description,
+    isComplete: task.isComplete,
+    startDate: parseISO(start),
+    endDate: parseISO(end),
+  };
+}
 
 export default databaseHelper;
