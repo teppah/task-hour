4bde460e229db132c1d5ffc7dfa6407e6e775714
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-16 10:59:40 -0400

    Add next-iron-session to API handler

--- lib/api/handler.ts
+++ lib/api/handler.ts
@@ -1,8 +1,18 @@
 import nc from "next-connect";
 import { NextApiRequest, NextApiResponse } from "next";
+import { ironSession, Session } from "next-iron-session";
+
+const session = ironSession({
+  cookieName: "th-session",
+  password: process.env.IRON_SESSION_PW,
+});
 
 function createHandler<T = any>() {
-  const handler = nc<NextApiRequest, NextApiResponse<T>>();
+  const handler = nc<
+    NextApiRequest & { session: Session },
+    NextApiResponse<T>
+  >();
+  handler.use(session);
   return handler;
 }
 
