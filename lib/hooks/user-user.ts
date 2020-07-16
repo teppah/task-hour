8f1e4cac7b2928b67f1cd3e3a7202fa0e9f38905
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-16 14:52:37 -0400

    Implement initial useUser hook

---
+++ lib/hooks/user-user.ts
@@ -0,0 +1,44 @@
+import useSWR from "swr";
+import fetcher from "lib/fetcher";
+import { useEffect } from "react";
+import User from "lib/user/User";
+import Router from "next/router";
+
+type UseUserOptions = {
+  // redirect the user if user is not present
+  redirectUrl?: string;
+  // if set to true, then hook will redirect to redirectUrl
+  // when user is found instead of not found
+  redirectIfFound?: boolean;
+};
+function useUser({
+  redirectUrl,
+  redirectIfFound = false,
+}: UseUserOptions = {}) {
+  const { data: user, mutate: mutateUser } = useSWR<User>(
+    `/api/auth/user`,
+    fetcher
+  );
+  useEffect(() => {
+    // if fetch is in progress
+    // OR
+    // if no redirect URL specified
+    if (!user || !redirectUrl) {
+      return;
+    }
+    // URL is set
+
+    // if no user found, and don't redirect after login
+    // OR
+    // if user is logged in, and needs to redirect if found
+    if (
+      (!user.isLoggedIn && !redirectIfFound) ||
+      (user.isLoggedIn && redirectIfFound)
+    ) {
+      Router.push(redirectUrl);
+    }
+  }, [user, redirectUrl, redirectIfFound]);
+  return { user, mutateUser };
+}
+
+export default useUser;
