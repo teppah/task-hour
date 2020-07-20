23d5b7656f0f73b0b2636b1c4c27b45086707fbd
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-20 15:54:18 -0400

    Implement sign up route

---
+++ pages/signup.tsx
@@ -0,0 +1,80 @@
+import { useFormik } from "formik";
+import useUser from "lib/hooks/use-user";
+import ky from "ky";
+import ClientSideUser from "lib/user/ClientSideUser";
+
+const SignUp = () => {
+  const { mutateUser } = useUser({
+    redirectUrl: "/app",
+    redirectIfFound: true,
+  });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values, helper) => {
+      const body = {
+        username: values.username,
+        email: values.email,
+        password: values.password,
+      };
+      console.log(`submitting`);
+      const signedUpUser = await ky
+        .post(`/api/auth/signup`, { json: body })
+        .json<ClientSideUser>();
+      mutateUser(signedUpUser);
+    },
+  });
+  return (
+    <div>
+      Sign Up
+      <form onSubmit={formik.handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <style jsx>{`
+        input {
+          @apply border border-black;
+        }
+        form {
+          @apply flex flex-col items-start;
+        }
+      `}</style>
+    </div>
+  );
+};
+export default SignUp;
