15fd0abc3d1b628cda88770fc335fa68a33dddb1
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-20 13:42:22 -0400

    Create intial signup route

--- lib/user/ServerSideUser.ts
+++ lib/user/ServerSideUser.ts
@@ -3,7 +3,7 @@ import BaseUser from "./BaseUser";
 // represents data that only server has access to, even if it's
 // stored in a cookie, since it's encrypted
 type ServerSideUser = BaseUser & {
-  passwordHash: "hash";
+  passwordHash: string;
 };
 
 export default ServerSideUser;
