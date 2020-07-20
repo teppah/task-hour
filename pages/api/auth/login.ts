b16c4cce80ad7ec4aab328692b569fc1da55f737
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-20 14:43:04 -0400

    Implement login route

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,14 +1,30 @@
 import createHandler from "lib/api/handler";
 import ClientSideUser from "lib/user/ClientSideUser";
+import argon2 from "argon2";
+import userHelper from "lib/api/user-helper";
 
 const handler = createHandler<ClientSideUser>();
 
 handler.post(async (req, res) => {
-  const { username } = req.body;
-  const user = { username, isLoggedIn: true };
-  req.session.set("user", user);
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).end("400 Missing Fields");
+    return;
+  }
+  const hashed = await argon2.hash(password);
+  const foundUser = await userHelper.getUser(email, hashed);
+  if (!foundUser) {
+    res.status(404).end(`404 User Not Found`);
+    return;
+  }
+  req.session.set("user", foundUser);
   await req.session.save();
-  res.json(user);
+  res.json({
+    userId: foundUser.userId,
+    username: foundUser.username,
+    email: foundUser.email,
+    isLoggedIn: true,
+  });
 });
 
 export default handler;
