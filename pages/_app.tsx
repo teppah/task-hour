c28f10b277707091fd96ffcb10b8de798e42ed01
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-23 14:25:04 -0400

    Move fonts to font-face declarations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "styles/tailwind.css";
+import "styles/global.css";
+import "styles/fonts.css";
 import "react-calendar/dist/Calendar.css";
-import store from "lib/client/redux/store";
-import { Provider } from "react-redux";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/light.css";
-import "styles/global.css";
+import store from "lib/client/redux/store";
+import { Provider } from "react-redux";
 
 const App = ({ Component, pageProps }) => {
   return (
