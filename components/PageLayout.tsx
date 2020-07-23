7c4acd8d7f66500d5544972ba3d633d2ba741d10
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-23 17:02:58 -0400

    Add header to layout

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,8 +1,13 @@
 import Navbar from "./Navbar";
+import Head from "next/head";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
+      <Head>
+        <title>TaskHour</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <nav>
         <Navbar />
       </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 import useUser from "lib/client/hooks/use-user";
 import Link from "next/link";
 import PageLayout from "components/PageLayout";
-import Head from "next/head";
 const Index = () => {
   const { user, isLoading } = useUser();
   return (
     <PageLayout>
-      <Head>
-        <title>Task Hour</title>
-      </Head>
-
       <div>
         <Link href="/app">
           <a>Go To Application</a>
