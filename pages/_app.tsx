772c163fcac8cd75fb95c4275857516f6b7851a6
Yi Feng Yang <yifeng@yfyang.dev>
2020-07-23 16:21:25 -0400

    Fix react-calendar font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import "styles/tailwind.css";
-import "styles/global.css";
 import "styles/fonts.css";
+import "styles/tailwind.css";
 import "react-calendar/dist/Calendar.css";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/light.css";
 import store from "lib/client/redux/store";
 import { Provider } from "react-redux";
+import "styles/global.css";
 
 const App = ({ Component, pageProps }) => {
   return (
